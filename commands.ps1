f81ed6986000861b53349662f20758666c22e85b
Brendan Forster <me@brendanforster.com>
2012-05-30 18:16:19 +1000

    getting there

--- commands.ps1
+++ commands.ps1
@@ -6,6 +6,7 @@ $build = Join-Path $location "build.cmd"
 $xunit = Join-Path  $location "\tools\xunit\xunit.console.clr4.x86.exe"
 $test_assembly = Join-Path  $location  "src\MarkPad.Tests\bin\Testing\MarkPad.Tests.dll"
 $assembly_info = Join-Path  $location "src\GlobalAssemblyInfo.cs"
+$wybuild = "C:\Program Files (x86)\wyBuild\wybuild.cmd.exe"
 
 function Update-Version
 {
@@ -16,9 +17,8 @@ function Update-Version
     Create-ReleaseDirectory $version
     Create-Package
     Bundle-Package  $version
-    Update-Manifest
+    Update-Manifest $version
 }
-
 
 function Run-Tests
 {
@@ -66,9 +66,26 @@ function Bundle-Package
     Copy-Item $binary $dir
 }
 
-function Update-Manifest()
+function Update-Manifest
 {
-    Log-Message "Update manifest"
+    Param([Parameter(Mandatory=$true)][string]$version)
+    Param([Parameter(Mandatory=$true)][string]$description)
+
+    $release_path = Join-Path (Get-Location) "tools\versions\$version\release.xml"
+    $config = Join-Path (Get-Location) "tools\wybuild.wyp"
+    $exe = Join-Path (Get-Location) "tools\versions\$version\MarkPad.exe"
+
+    Log-Message "Generating new manifest to $release_path"
+
+    $release = "<?xml version=`"1.0`" encoding=`"utf-8`"?><Versions><AddVersion><Version>$version</Version><Changes>$description</Changes><InheritPrevRegistry /><InheritPrevActions /><Files dir=`"basedir`"><File source=`"$exe`" /></Files></AddVersion></Versions>";
+
+    set-content -Value $release $release_path
+
+    Log-Message "Applying new version to wyBuild config"
+    CMD \C "$wybuild $config /bu -add=`"newversions.xml`""
+
+    Log-Message "Building deltas"
+    CMD \C "$wybuild $config /bwu"
 }
 
 function Log-Message ($message)
@@ -77,9 +94,9 @@ function Log-Message ($message)
 }
 
 switch ($Action) 
-    { 
-        "Run-Tests" { Run-Tests } 
-        "Create-Package" { Create-Package }
-        "Update-Version" { Update-Version }
-        default { }
-    }
+{ 
+    "Run-Tests" { Run-Tests } 
+    "Create-Package" { Create-Package }
+    "Update-Version" { Update-Version }
+    default { }
+}
